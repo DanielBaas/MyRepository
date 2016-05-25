e116b37cd106e3634a0bb6269469caa9a6f38735
DanielBaas <danfer742@gmail.com>
2016-05-25 16:39:29 -0500

    MI PRIMER COMMIT HOLA MUNDO

--- HellouWorld/src/hellouworld/HellouWorld.java
+++ HellouWorld/src/hellouworld/HellouWorld.java
@@ -16,7 +16,7 @@ public class HellouWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola a todos");
+        System.out.println("Hola a todos mis amigos");
     }
     
 }
